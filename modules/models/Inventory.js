1681844200451fd1ff01332f251ae60d9728e8ef
RASMAP <ras.seidi65@gmail.com>
2021-05-19 01:57:14 -0700

    .

--- modules/models/Inventory.js
+++ modules/models/Inventory.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt');
 const InventorySchema = new Schema({
     productID: { type:mongoose.Schema.ObjectId,require},
     count: { type: Number },
-    // date: { type: String },
-    // time:{type: String,default:'00:00:00'},
+    date: { type: String },
+    time:{type: String,default:'00:00:00'},
 },{timestamps:true,toJSON:{virtuals:true}});
 InventorySchema.virtual('product',{
     ref:'Product',
