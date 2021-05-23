f4888ff2e638a5d96cab50a098cb2c8ca9bd859b
RASMAP <ras.seidi65@gmail.com>
2021-05-22 23:41:12 -0700

    .

--- modules/controllers/api/v1/admin/ProductsController.js
+++ modules/controllers/api/v1/admin/ProductsController.js
@@ -1,7 +1,7 @@
 const Controller = require(`${config.path.controller}/Controller`);
 module.exports = new class ProductsController extends Controller {
     index(req, res) {
-        this.model.Products.find({}).sort({name: -1}).populate({
+        this.model.Products.find({}).sort({createdAt: -1}).populate({
             path: 'Category SubCategory  Inventory ProductFeature',
             populate: [{
                 path: 'FeaturesValue',
