c866c34493eb705d908dbd648f7aa280b1d913d3
RASMAP <ras.seidi65@gmail.com>
2021-05-25 05:07:32 -0700

    .

--- modules/models/payment.js
+++ modules/models/payment.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const timestamps = require('mongoose-timestamp');
 const mongoosePaginate = require('mongoose-paginate');
-
-
 const PaymentSchema = new Schema({
     userID: {type:mongoose.Schema.ObjectId, required: true },//کد مشتری
     resNumber:{ type: String, required: true },//شناسه پرداخت
@@ -12,6 +10,6 @@ const PaymentSchema = new Schema({
     statusPayment:{type: String, default: 'ناموفق'},//وضعیت تراکنش
     date: { type: String, required: true },//تاریخ
     time: { type: String, required: true }//ساعت
-});
+}, {toJSON: {virtuals: true}});
 
 module.exports = mongoose.model(' Payment', PaymentSchema);
